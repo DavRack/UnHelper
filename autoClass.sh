c39d94c6d1bb43a57ca5a550ad272914f1a7ceb7
david <davlondonomon@unal.edu.co>
2019-10-03 22:13:46 -0500

    class helper

--- autoClass.sh
+++ autoClass.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 semestreActual="s8"
 newClassNote(){
-    workDir="$HOME/Documentos/unal/$semestreActual/$1/notas/"
+    workDir="$HOME/Documentos/unal/$semestreActual/$1/notas"
     #conseguir la ultima clase
-    ultimaClase=$( ls $workDir -C --sort=t | awk '{print $1}'| sed "s/clase//g" | sed "s/\.rmd//g" | sed "s/\.pdf//g" )
+    ultimaClase=$( ls $workDir/ -C --sort=t | awk '{print $1}'| sed "s/clase//g" | sed "s/\.rmd//g" | sed "s/\.pdf//g" )
     nuevaClase=$(($ultimaClase+1))
-    nvim "clase$nuevaClase.rmd"
+    nvim "$workDir/clase$nuevaClase.rmd"
 }
 if [ $# == 0 ]; then
     echo "Faltan argumentos"
